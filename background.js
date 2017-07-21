a94954579045f70adee906f80aacb540328356fa
M!nified <gora.pub@gmail.com>
2017-07-21 14:29:05 +0200

    display popup

--- background.js
+++ background.js
@@ -8,31 +8,72 @@ chrome.browserAction.onClicked.addListener(tab => {
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // console.debug(tabId, changeInfo, tab);
     if (changeInfo.status === 'complete') {
-        chrome.tabs.executeScript(tab.id, {
-            file: "./dist/listener.js",
-            runAt: 'document_start'
-        })
+        try {
+            chrome.tabs.executeScript(tab.id, {
+                file: "./dist/listener.js",
+                runAt: 'document_start'
+            }, result => {
+                if (chrome.runtime.lastError) {
+                    //
+                }
+            })
+        } catch (ex) {
+            console.warn(ex);
+        }
     }
 });
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // console.debug(message, sender, sendResponse);
     console.debug(message.event.altKey, message.event.target);
+    sendToPopup({ target: message.event.target });
 });
 
 var popUpId = null;
 var sendToPopup = message => {
-    let window = null;
-    if (popUpId !== null) {
-        window = chrome.windows.get(popUpId);
-    }
-    if (!popUpId || !window) {
-        chrome.windows.create({
-            url: './html/   display_popup.html',
-            type: 'popup'
-        }, newWindow => {
-            popUpId = newWindow.id;
-            window = newWindow;
+    console.debug(popUpId);
+    ensurePopupPresence(popUpId)
+        .then(updatedPopupId => {
+            popUpId = updatedPopupId;
+            chrome.runtime.sendMessage(message);
         })
-    }
-    chrome.runtime.sendMessage();
+        .catch(exception => {
+            console.warn(exception);
+        })
+};
+var popupScheduled = false;
+var ensurePopupPresence = windowId => {
+    return new Promise((resolve, reject) => {
+        if (popupScheduled) {
+            reject();
+        } else {
+            popupScheduled = true;
+            tryToGetWindow(windowId)
+                .then(window => {
+                    if (!window) {
+                        chrome.windows.create({
+                            url: './html/display_popup.html',
+                            type: 'popup'
+                        }, window => {
+                            popupScheduled = false;
+                            console.warn(window.id);
+                            resolve(window.id);
+                        });
+                    } else {
+                        popupScheduled = false;
+                        resolve(window.id);
+                    }
+                });
+        }
+    });
+}
+var tryToGetWindow = windowId => {
+    return new Promise((resolve, reject) => {
+        if (!windowId) {
+            resolve(false);
+        } else {
+            chrome.windows.get(windowId, window => {
+                resolve(window);
+            })
+        }
+    });
 }
